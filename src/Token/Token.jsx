fb8545549d7301eb1faeaa7851194f48e92b127d
Md Hazrat Ali <md.hazratali4510@gmail.com>
2022-04-20 23:35:13 +0600

    first commit

---
+++ src/Token/Token.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import token_info from "../assets/data/token-info";
+import imageShapes from "../assets/images/shadow-image/token-shapes.png";
+import image from "../assets/images/token/token.png";
+import SectionHeader from "../components/SectionHeader";
+import "./Token.sass";
+const Token = () => {
+	return (
+		<section className="tokenSection position-relative pb-150" id="token">
+			<div className="tokenImageShape">
+				<img src={imageShapes} alt="token" />
+			</div>
+			<Container className="position-relative">
+				<div className="tokenWrapper">
+					<div className="tokenImage">
+						<img src={image} alt="token" />
+					</div>
+					<div className="tokenContent">
+						<SectionHeader
+							cate="Token"
+							title="Token Destribution"
+							text="The initial token distribution was completed on April 4, 2021 as an airdrop to thousands of people from all over the world as ROPE, and as mentioned will undergo a 1:1 token swap to PHASE on February 1st 2022."
+						/>
+						<div className="aboutContent text-start">
+							<p>
+								The remaining tokens that aren't listed as 'existing
+								ROPE supply' will be vested through the Bonfida Vesting
+								Contract, and the softened cap will serve as a means to
+								safeguard the index pool from depletion via a mint/burn
+								mechanism—a feature that can increase or decrease the
+								total supply when utilized. This mechanism will be
+								described in greater detail later on in this paper.
+							</p>
+						</div>
+						<ul className="tokenInfo">
+							{token_info.length > 0 &&
+								token_info.map(({ txt }, index) => (
+									<li key={index}>{txt}</li>
+								))}
+						</ul>
+					</div>
+				</div>
+			</Container>
+		</section>
+	);
+};
+
+export default Token;
